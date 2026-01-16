d53ceea016f0cb092d64084da57462b5a7a1f407
kaushikp092 <kaushikpatil092@gmail.com>
2026-01-16 16:47:09 +0530

    timer added to local storage

--- src/components/TimerControls.jsx
+++ src/components/TimerControls.jsx
@@ -1,8 +1,19 @@
+import React, { useState, useEffect, useRef } from 'react'
+
 const TimerControls = ({ isRunning, onReset, onToggle }) => {
     
+    const startButtonRef = useRef(null);
+
+    useEffect(() => {
+        if(startButtonRef.current){
+            startButtonRef.current.focus();
+        }
+    }, [])
+
    return (
       <div>
          <button
+            ref={startButtonRef}
             onClick={onToggle}
             className="mt-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 mr-3"
          >
